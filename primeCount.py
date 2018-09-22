4c7c3043ef34cc02544e1ae9a732de195f93236d
damianMietus <damianmietus1@gmail.com>
2018-09-21 21:41:36 -0400

    Created functions and main method
    Created a function that counts prime numbers, along with a main method that gets user input and error checks it.

---
+++ primeCount.py
@@ -0,0 +1,52 @@
+# Prime Number Counter 
+def primeNumberFinder(size):
+    pnArray = [2, 3, 5]
+    i = 7;
+    j = 0;
+    pnFlag = True
+    for i in range (7, size):      
+      pnFlag = True
+      for j in range(0, len(pnArray)-1):      
+        if i%pnArray[j] == 0:
+          pnFlag = False
+          break
+        j+1 
+      if pnFlag == True:
+        pnArray.append(i)
+      i + 2
+    return pnArray
+
+def digitCounter(number):
+	digits = 0
+	while (number > 0):
+		digits = digits + 1
+		number = number//10
+	return digits
+
+def invalidDigitCountError(integer):
+	print ("invalid entry. You're input must have", integer, "digits in length. Please input again: ")
+	return
+    
+#Main
+i = 0
+j = 0
+digitFlag = True
+number = int(input("Enter a number for the Prime Grid: "));
+grid = [[0 for x in range(number)] for y in range(number)]
+for i in range(0, number):
+	grid[i] = int(input(""));
+	print (digitCounter(grid[i]))
+	if digitCounter(grid[i]) != number:		
+		digitFlag = False
+		while (digitFlag == False):
+			invalidDigitCountError(number)
+			grid[i] = int(input(""));
+			if digitCounter(grid[i]) == number:
+				digitFlag = True
+		
+
+
+print (grid)
+#primeNumbers = primeNumberFinder(1000)
+#print (primeNumbers[0], primeNumbers[1])
+#print (len(primeNumbers))
