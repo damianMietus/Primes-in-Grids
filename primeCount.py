bf907a2aa4655db135be88f03fac17e66a57d527
damianMietus <damianmietus1@gmail.com>
2018-09-24 10:47:55 -0400

    Additional Update
    Has everything but a few diagonal edge cases working.

--- primeCount.py
+++ primeCount.py
@@ -26,7 +26,69 @@ def digitCounter(number):
 def invalidDigitCountError(integer):
 	print ("invalid entry. You're input must have", integer, "digits in length. Please input again: ")
 	return
+	
+def reverseString(string):
+	return string[::-1]
     
+def countPrimeLine(line, pnFound, pnList, size):
+	i = 0
+	intSize = 1
+	addBool = True
+	while (intSize < size+1):
+		
+		for i in range(0, size-intSize+1):
+			#print ("i = ", i)
+			#get the part of line to check
+			temp = line[i:i+intSize]
+			addBool = True
+			
+			#check if it is not in our prime list
+			if temp in str(pnFound):
+				addBool = False
+				
+			#check if our number is prime
+			if addBool == True:
+				if temp in pnList:
+					pnFound.append(temp)
+					#print("Found one")
+			
+			i = i + 1
+	
+		intSize = intSize + 1
+		#print ("Reached the end of the while loop")
+	
+	
+	return pnFound
+	
+def	getVerticalLine(grid, number, i):
+	tempVertical = ""
+	tempStr = ""
+	for j in range (0, number):
+		tempStr = grid[j]
+		tempVertical = tempVertical + tempStr[i]	
+
+	return tempVertical
+	
+def getRightDiagonal(grid, number):
+	tempDiag = ""
+	tempStr = ""
+	j = 0
+	for i in range (0, number):
+		tempStr = grid[i]
+		tempDiag = tempDiag + tempStr[j]
+		j = j + 1
+	return tempDiag
+	
+def getLeftDiagonal(grid, number):
+	tempDiag = ""
+	tempStr = ""
+	j = number-1
+	for i in range (0, number):
+		tempStr = grid[i]
+		tempDiag = tempDiag + tempStr[j]
+		j = j - 1
+	return tempDiag
+		
 #Main
 i = 0
 j = 0
@@ -35,7 +97,6 @@ number = int(input("Enter a number for the Prime Grid: "));
 grid = [[0 for x in range(number)] for y in range(number)]
 for i in range(0, number):
 	grid[i] = int(input(""));
-	print (digitCounter(grid[i]))
 	if digitCounter(grid[i]) != number:		
 		digitFlag = False
 		while (digitFlag == False):
@@ -46,7 +107,57 @@ for i in range(0, number):
 		
 
 
-print (grid)
-#primeNumbers = primeNumberFinder(1000)
-#print (primeNumbers[0], primeNumbers[1])
-#print (len(primeNumbers))
+pnGrid = []		
+for item in grid:
+	pnGrid.append(str(item))
+print (pnGrid)
+
+
+
+
+primeNumbersInt = primeNumberFinder(9999)
+primeNumbersStr = []
+for item in primeNumbersInt:
+	primeNumbersStr.append(str(item))
+
+#print (primeNumbersStr)
+
+primeNumbersFound = []
+tempVertical = ""
+tempRightDiagonal = ""
+for i in range (0, number):
+	#Horizontal
+	primeNumbersFound = countPrimeLine(pnGrid[i], primeNumbersFound, primeNumbersStr, number);
+	tempReverse = reverseString(pnGrid[i])
+	primeNumbersFound = countPrimeLine(tempReverse, primeNumbersFound, primeNumbersStr, number);
+	
+	#Vertical
+	tempVertical = getVerticalLine(pnGrid, number, i)
+	primeNumbersFound = countPrimeLine(tempVertical, primeNumbersFound, primeNumbersStr, number);
+	tempReverse = reverseString(tempVertical)
+	primeNumbersFound = countPrimeLine(tempReverse, primeNumbersFound, primeNumbersStr, number);
+	
+
+	print("Vertical = ", tempVertical)
+	
+
+#Diagonal
+tempRightDiagonal = getRightDiagonal(pnGrid, number)
+primeNumbersFound = countPrimeLine(tempRightDiagonal, primeNumbersFound, primeNumbersStr, number);
+#print("Right Diag =", tempRightDiagonal)
+tempReverse = reverseString(tempRightDiagonal)
+primeNumbersFound = countPrimeLine(tempReverse, primeNumbersFound, primeNumbersStr, number);
+#Check from center to up-right 
+#for i in range(0, number):
+
+tempLeftDiagonal = getLeftDiagonal(pnGrid, number)
+primeNumbersFound = countPrimeLine(tempLeftDiagonal, primeNumbersFound, primeNumbersStr, number);
+#print("Left Diag =", tempLeftDiagonal)
+tempReverse = reverseString(tempLeftDiagonal)
+primeNumbersFound = countPrimeLine(tempReverse, primeNumbersFound, primeNumbersStr, number);
+
+#primeNumbersFound = countPrimeLine(pnGrid[0], primeNumbersFound, primeNumbersStr, number);
+#primeNumbersFound = countPrimeLine(pnGrid[1], primeNumbersFound, primeNumbersStr, number);
+
+print ("Total Unique Prime Numbers: ", len(primeNumbersFound))
+print (primeNumbersFound)
